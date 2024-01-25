bbd1ce94fb3c75018f73a22d0814f6977065588e
mdmuzammil007 <155004647+mdmuzammil007@users.noreply.github.com>
2024-01-25 22:48:22 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,26 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event TaxPaid(uint256 amount);
 
-    constructor(uint initBalance) payable {
+    constructor(uint256 initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
 
         // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
@@ -40,7 +39,7 @@ contract Assessment {
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -56,5 +55,35 @@ contract Assessment {
 
         // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function checkPassword(string memory _password) public pure returns (bool) {
+        return keccak256(abi.encodePacked(_password)) == keccak256(abi.encodePacked("abcd"));
+    }
+
+    // Function to calculate and pay taxes
+    function payTaxes() public {
+        // make sure this is the owner
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // calculate tax as 10% of the current balance
+        uint256 taxAmount = (balance * 10) / 100;
+
+        // subtract tax from the balance
+        balance -= taxAmount;
+
+        // emit the event
+        emit TaxPaid(taxAmount);
+    }
+
+    // Function to get the tax amount without actually paying it
+    function getTaxAmount() public view returns (uint256) {
+        // calculate tax as 10% of the current balance
+        return (balance * 10) / 100;
+    }
+
+    // Function for auditing purposes to retrieve relevant contract details
+    function auditContract() public view returns (address, uint256) {
+        return (owner, balance);
     }
 }
