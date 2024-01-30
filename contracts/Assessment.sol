e2d08996f13ed831b852565cdca43751335562ec
mdmuzammil007 <155004647+mdmuzammil007@users.noreply.github.com>
2024-01-30 16:13:40 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -4,31 +4,28 @@ pragma solidity ^0.8.9;
 contract Assessment {
     address payable public owner;
     uint256 public balance;
-    bool public accountClosed;
-    string public accountDetails; // Store account details as a string
+    string private password = "meta"; // Set password to "meta"
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
-    event AccountClosed(bool closed);
-    event AccountReopened(bool reopened);
+    event BalanceDoubled(uint256 newBalance);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
-        accountClosed = false;
     }
 
-    function getBalance() public view returns(uint256) {
+    function getBalance() public view returns(uint256){
         return balance;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "You are not the owner of this account");
-        _;
+    function setPassword(string memory _newPassword) public {
+        require(msg.sender == owner, "Only owner can set password");
+        password = _newPassword;
     }
 
-    function deposit(uint256 _amount) public payable onlyOwner {
-        require(!accountClosed, "Account is closed");
+    function deposit(uint256 _amount, string memory _enteredPassword) public payable {
+        require(keccak256(abi.encodePacked(_enteredPassword)) == keccak256(abi.encodePacked(password)), "Invalid password");
         uint _previousBalance = balance;
 
         // perform transaction
@@ -41,11 +38,20 @@ contract Assessment {
         emit Deposit(_amount);
     }
 
-    // custom error
+    function doubleBalance(string memory _enteredPassword) public {
+        require(keccak256(abi.encodePacked(_enteredPassword)) == keccak256(abi.encodePacked(password)), "Invalid password");
+
+        // double the balance
+        balance *= 2;
+
+        // emit the event
+        emit BalanceDoubled(balance);
+    }
+
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public onlyOwner {
-        require(!accountClosed, "Account is closed");
+    function withdraw(uint256 _withdrawAmount, string memory _enteredPassword) public {
+        require(keccak256(abi.encodePacked(_enteredPassword)) == keccak256(abi.encodePacked(password)), "Invalid password");
         uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
@@ -62,47 +68,5 @@ contract Assessment {
 
         // emit the event
         emit Withdraw(_withdrawAmount);
-    }
-
-    function closeAccount() public onlyOwner {
-        accountClosed = true;
-        emit AccountClosed(true);
-    }
-
-    function reopenAccount() public onlyOwner {
-        accountClosed = false;
-        emit AccountReopened(true);
-    }
-
-    function fetchBalance() public view returns (uint256) {
-        return balance;
-    }
-
-    // Function to set account details and generate QR code
-    function setAccountDetails(
-        string memory _name,
-        string memory _education,
-        string memory _accountOpenedDate,
-        string memory _panNumber,
-        string memory _swiftCode,
-        string memory _customerId,
-        string memory _crn
-    ) public onlyOwner {
-        accountDetails = string(
-            abi.encodePacked(
-                "Name: ", _name, "\n",
-                "Education: ", _education, "\n",
-                "Account Opened Date: ", _accountOpenedDate, "\n",
-                "PAN Number: ", _panNumber, "\n",
-                "Swift Code: ", _swiftCode, "\n",
-                "Customer ID: ", _customerId, "\n",
-                "CRN: ", _crn
-            )
-        );
-    }
-
-    // Function to get the stored account details
-    function getAccountDetails() public view returns (string memory) {
-        return accountDetails;
     }
 }
